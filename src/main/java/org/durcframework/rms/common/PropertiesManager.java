d93ae2a983e0e5382fb25fee81ece4c6260481c8
durcframework <thc8775@163.com>
2014-08-06 11:24:46 +0800

    完善PropertiesManager功能

--- src/main/java/org/durcframework/rms/common/PropertiesManager.java
+++ src/main/java/org/durcframework/rms/common/PropertiesManager.java
@@ -1,6 +1,8 @@
 package org.durcframework.rms.common;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import org.springframework.core.io.ClassPathResource;
@@ -13,7 +15,8 @@ import org.springframework.core.io.support.PropertiesLoaderUtils;
 public enum PropertiesManager {
 	instance;
 
-	private Properties props;
+	private Map<String, Properties> propMap = new HashMap<String, Properties>();
+	private String defProperties = "config.properties"; // 默认的属性文件
 
 	public static PropertiesManager getInstance() {
 		return instance;
@@ -21,13 +24,32 @@ public enum PropertiesManager {
 
 	PropertiesManager() {
 		// 默认读取config.properties
-		Resource resource = new ClassPathResource("/config.properties");
-		try {
-			props = PropertiesLoaderUtils.loadProperties(resource);
-		} catch (IOException e) {
-			props = new Properties();
-			e.printStackTrace();
+		loadProp(defProperties);
+	}
+
+	private Properties loadProp(String fileName) {
+		Properties props = propMap.get(fileName);
+
+		if (props == null) {
+			Resource resource = new ClassPathResource(fileName);
+			try {
+				props = PropertiesLoaderUtils.loadProperties(resource);
+				propMap.put(fileName, props);
+			} catch (IOException e) {
+				props = new Properties();
+				e.printStackTrace();
+			}
 		}
+
+		return props;
+	}
+
+	private String getValue(String key, String fileName) {
+		Properties prop = propMap.get(fileName);
+		if (prop == null) {
+			prop = loadProp(fileName);
+		}
+		return prop.getProperty(key);
 	}
 
 	/**
@@ -37,7 +59,17 @@ public enum PropertiesManager {
 	 * @return
 	 */
 	public String get(String key) {
-		return props.getProperty(key);
+		return getValue(key, defProperties);
+	}
+
+	/**
+	 * 根据key和文件名获取值
+	 * @param key
+	 * @param fileName 文件名,必须放在classpath根目录下
+	 * @return
+	 */
+	public String get(String key, String fileName) {
+		return getValue(key, fileName);
 	}
 
 	public static void main(String[] args) {
